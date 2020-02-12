d3ef70368d04f2869176c35f3034cc5fc92fea8e
Paulwhent <48323848+Paulwhent@users.noreply.github.com>
2020-02-12 15:39:18 +0000

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'root-directive',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-first-project';
+
+  name:string = "Paul";
+  age:number = 108;
+}
